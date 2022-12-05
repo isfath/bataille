34482f71945d43a278da98fbc0677d408ec0e276
Guillaume Huysmans <ghuysmans99@gmail.com>
2022-12-05 20:37:20 +0100

    Comparaison via un nombre

--- ocaml/bataille.ml
+++ ocaml/bataille.ml
@@ -4,3 +4,26 @@ type figure = V | D | R | A
 type valeur = Point of point | Figure of figure
 type carte = valeur * couleur
 type carte' = Joker | Simple of carte
+
+let nombre x =
+  match x with
+  | Joker -> 15
+  | Simple s ->
+    match s with
+    | valeur, _couleur ->
+      match valeur with
+      | Point C2 -> 2
+      | Point C3 -> 3
+      | Point C4 -> 4
+      | Point C5 -> 5
+      | Point C6 -> 6
+      | Point C7 -> 7
+      | Point C8 -> 8
+      | Point C9 -> 9
+      | Point C10 -> 10
+      | Figure V -> 11
+      | Figure D -> 12
+      | Figure R -> 13
+      | Figure A -> 14
+
+let (<<) c c' = nombre c < nombre c'
